ed1d93b8f7820c08c8aaed4890b325f4acbe883b
ayilaemad <ae07352@st.habib.edu.pk>
2023-04-12 14:40:13 +0500

    Update Merkle Tree.py

--- Merkle Tree.py
+++ Merkle Tree.py
@@ -3,9 +3,10 @@ import hashlib
 class Node:
     def __init__(self, value):
         self.value = value
-        self.hash = hashlib.sha256(value.encode()).hexdigest()
+        self.hash = hashlib.sha256(value.encode('utf-8')).hexdigest()
         self.left = None
         self.right = None
+        self.parent = None
 
 class MerkleTree:
     def __init__(self):
@@ -13,6 +14,29 @@ class MerkleTree:
 
     def insert(self,value):
         # adds a new node to the tree
+        node = Node(value)
+        print(node.value)
+        if not self.root:
+            self.root = node
+        else:
+            current_node = self.root
+            while True:
+                if value < current_node.value:
+                    if current_node.left is None:
+                        current_node.left = node
+                        node.parent = current_node
+                        break
+                    else:
+                        current_node = current_node.left
+                else:
+                    if current_node.right is None:
+                        current_node.right = node
+                        node.parent = current_node
+                        break
+                    else:
+                        current_node = current_node.right
+            self.rehash(node)
+            print("rehashed")
         pass
 
     def delete(self,value):
@@ -25,8 +49,22 @@ class MerkleTree:
         # returns True if correct, False if incorrect
         pass
 
-    def rehash(self):
+    def rehash(self,node):
         # updates the hash values of each node 
+        while node:
+            if node.left:
+                left_hash = node.left.hash
+            else: 
+                left_hash = ""
+
+            if node.right:
+                right_hash = node.right.hash
+            else: 
+                right_hash = ""
+                
+            node.hash = hashlib.sha256((left_hash + right_hash).encode('utf-8')).hexdigest()
+            print("node hash: ",node.hash)
+            node = node.parent
         pass
 
     def get_proof(self,value):
@@ -37,3 +75,12 @@ class MerkleTree:
     def print_tree(self):
         # prints the contents of the tree 
         pass
+
+tree = MerkleTree()
+
+# Insert some values into the tree
+tree.insert("apple")
+tree.insert("banana")
+tree.insert("cherry")
+tree.insert("date")
+tree.insert("elderberry")    
