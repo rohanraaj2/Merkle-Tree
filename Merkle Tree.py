e9de67c814360e71f3c5c8af9e934be55f8be4fc
ayilaemad <ae07352@st.habib.edu.pk>
2023-04-14 00:21:40 +0500

    Update Merkle Tree.py

--- Merkle Tree.py
+++ Merkle Tree.py
@@ -7,26 +7,31 @@ class Node:
         self.hashval = hashval                          # repesents hash value of node
         self.value = value                              # represents content of node
 
-    def hash(self, val) -> str:
-        # in the initial stage, val represents the content. in later stages, val represents the hashval that is being rehashed 
-        return hashlib.sha256(val.encode('utf-8')).hexdigest()    # returns hash value of node
-
 class MerkleTree:
     def __init__(self, hashes: list):
         self.buildTree(hashes)
+
+    def hash(self, val : str) -> str:
+        return hashlib.sha256(val.encode('utf-8')).hexdigest()    # returns hash value of provided node    
 
     def buildTree(self, hashes: list) -> None:
         # builds a tree from the dataset 
 
         nodes = []                                          # creates the initial list of nodes that will be stored in the merkle tree
         for val in hashes:
-            nodes.append(Node(None, None, Node.hash(val), val))
+            print(val)
+            print("node hash", self.hash(val))
+            node = Node(None,None,self.hash(val),val)
+            nodes.append(node)  
+        print("nodes: ", nodes) 
 
         if len(nodes) % 2 != 0:                             # if no of nodes are odd, duplicate the last node to make it even
             nodes.append(nodes[-1])
+            print("dup")
 
         self.root = self._buildTree(nodes)                  # resulting tree is stored in the root of the MerkleTree object
-        self.print_tree()
+        print("root: ", self.root.value)
+        self.print_tree(self.root)
 
     def insert(self, value, hashes : list):
         # adds values to the list
@@ -64,43 +69,10 @@ class MerkleTree:
         # returns True if correct, False if incorrect
         pass
 
-    def rehash(self,node):
-        # updates the hash values of each node 
-        while node:
-            if node.left:
-                left_hash = node.left.hash
-            else: 
-                left_hash = ""
-
-            if node.right:
-                right_hash = node.right.hash
-            else: 
-                right_hash = ""
-
-            node.hash = hashlib.sha256((left_hash + right_hash).encode('utf-8')).hexdigest()
-            # print("node hash: ",node.hash)
-            node = node.parent
-        pass
-
     def get_proof(self,value):
         # proves that the value exists in the tree
         # returns the proof for the given value
         pass
-
-    # def print_tree(self, node= None, indent=0):
-    #     # prints the contents of the tree 
-    #     if node is None:
-    #         node = self.root
-
-    #     print(" " * indent, end="")
-    #     #print(f"{node.value} ({node.hash})")
-    #     print(f"{node.value}")
-
-    #     if node.left:
-    #         self.print_tree(node.left, indent+2)
-    #     if node.right:
-    #         self.print_tree(node.right, indent+2)
-    #     pass
 
     def print_tree(self, node: Node) -> None: 
         # prints the values of the tree
@@ -112,14 +84,15 @@ class MerkleTree:
     # helper functions
     def _buildTree(self, nodes: list[Node]) -> Node:
         
-        if len(nodes) % 2 != 0:                             # if no of nodes are odd, duplicate the last node to make it even
+        if len(nodes) % 2 != 0:                             # if no. of nodes are odd, duplicate the last node to make it even
             nodes.append(nodes[-1])
+            print("duplicating")
 
         half: int = len(nodes) // 2                         # to be used to split the tree for obtaining left right branches
  
         # if only 2 elements exist, the new node is the hash of the hashes of the two
         if len(nodes) == 2:                                 
-            node = Node(nodes[0], nodes[1], Node.hash(nodes[0].hashval + nodes[1].hashval), nodes[0].value+" "+nodes[1].value)
+            node = Node(nodes[0], nodes[1], self.hash(nodes[0].hashval + nodes[1].hashval), nodes[0].value+" "+nodes[1].value)
             return node
  
         # Recursively build the left and right subtrees
@@ -127,7 +100,7 @@ class MerkleTree:
         right = self._buildTree(nodes[half:])
 
         # Calculate the hash and value of the current node based on the hashes and values of its children
-        hashval = Node.hash(left.hashval + right.hashval)
+        hashval = self.hash(left.hashval + right.hashval)
         value = f'{left.value}+{right.value}'
 
         # Create and return a new node with the calculated hash and value
@@ -219,13 +192,5 @@ class MerkleTree:
 
         return self._find_parent(node.right, child)
 
-hashes = ["hash1", "hash2", "hash3", "hash4", "hash5"]\
-
+hashes = ["hash1", "hash2", "hash3", "hash4"]
 merkle_tree = MerkleTree(hashes)
-nodes = [
-Node(None, None, Node.hash("hash1"), "hash1"),
-Node(None, None, Node.hash("hash2"), "hash2"),
-Node(None, None, Node.hash("hash3"), "hash3"),
-Node(None, None, Node.hash("hash4"), "hash4"),
-Node(None, None, Node.hash("hash5"), "hash5")
-]
