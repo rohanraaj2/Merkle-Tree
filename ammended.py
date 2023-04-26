7b70d6a389bf96cf18a23fe65aaff74819acc590
ayilaemad <ae07352@st.habib.edu.pk>
2023-04-26 22:03:10 +0500

    Update ammended.py

--- ammended.py
+++ ammended.py
@@ -3,7 +3,7 @@ import hashlib
 class Merkletree:
 
     def __init__(self, words:list) -> None:
-        self.build_tree(words)
+        self.tree = self.build_tree(words)
 
     def hash(self, val : str) -> str:
         return hashlib.sha256(val.encode('utf-8')).hexdigest()    # returns hash value of provided node    
@@ -20,25 +20,9 @@ class Merkletree:
         
         tree = [hashes]                             # creates a tree and adds the list of hashes as the first index
         
-        self.generate(hashes, tree)                 # calls this function so that it can create the remainign levels of hashes
-        print("tree", tree)
+        self._generate(hashes, tree)                 # calls this function so that it can create the remainign levels of hashes
+        # print("tree", tree)
         return tree
-
-    def generate(self,hashes, tree):
-        if len(hashes) == 1:                            # if only one hash then return that only
-            return hashes
-            
-        self.ensureEven(hashes)                         # check if need for duplication
-        combinedHashes = []
-
-        for i in range(0, len(hashes), 2):                          # use every alternate node and concatenate it to create the parent hash
-            hashesConcatenated = hashes[i] + hashes[i + 1]
-            hash = self.hash(hashesConcatenated)
-            combinedHashes.append(hash)                             
-
-        tree.append(combinedHashes)                                 # append the levels of hashes to the tree
-        print(tree)
-        return self.generate(combinedHashes, tree)
 
     def generateMerkleRoot(self,hashes):
         # returns the merkle root 
@@ -46,7 +30,7 @@ class Merkletree:
         if not hashes or len(hashes) == 0:
             return ''
         
-        self.ensureEven(hashes)
+        self._ensureEven(hashes)
         combinedHashes = []
 
         for i in range(0, len(hashes), 2):
@@ -68,31 +52,66 @@ class Merkletree:
             self.tree[0].append(hash)
             self.generateMerkleTree(self.tree[0])
 
-    def generateMerkleProof(self, hash, hashes):
-        if not hash or not hashes or len(hashes) == 0:
-            return None
-        
-        tree = self.generateMerkleTree(hashes)
-        merkleProof = [{'hash': hash,'direction': self.getLeafNodeDirectionInMerkleTree(hash, tree) }]
-        hashIndex = tree[0].index(hash)
+    def generate_proof(self, hash, hashes):
+        # returns the proof for the inclusivity of a hash
+        if hash not in hashes:                                                  # inclusivity failed 
+            return []
+     
+        merkleProof = [{'hash': hash,'direction': self._get_direction(hash, self.tree) }]
+        hashIndex = self.tree[0].index(hash)
 
-        for level in range(len(tree) - 1):
+        for level in range(len(self.tree) - 1):                                  # traversing through the levels
             isLeftChild = hashIndex % 2 == 0
             siblingDirection = 'right' if isLeftChild else 'left'
             siblingIndex = hashIndex + 1 if isLeftChild else hashIndex - 1
-            siblingNode = {'hash': tree[level][siblingIndex],'direction': siblingDirection}
+            siblingNode = {'hash': self.tree[level][siblingIndex],'direction': siblingDirection}
+            merkleProof.append(siblingNode)
+            hashIndex = hashIndex // 2
+
+        return merkleProof
+    
+    def generate_proof_using_value(self, value, hashes):
+        # returns the proof for the inclusivity of a value
+        hash = self.hash(value)
+        if hash not in hashes:                                                  # inclusivity failed 
+            return []
+     
+        merkleProof = [{'hash': hash,'direction': self._get_direction(hash, self.tree) }]
+        hashIndex = self.tree[0].index(hash)
+
+        for level in range(len(self.tree) - 1):                                  # traversing through the levels
+            isLeftChild = hashIndex % 2 == 0
+            siblingDirection = 'right' if isLeftChild else 'left'
+            siblingIndex = hashIndex + 1 if isLeftChild else hashIndex - 1
+            siblingNode = {'hash': self.tree[level][siblingIndex],'direction': siblingDirection}
             merkleProof.append(siblingNode)
             hashIndex = hashIndex // 2
 
         return merkleProof
     
     def verify(self, hash, hashes):
-        if self.generateMerkleProof(self, hash, hashes) == None:
+        if self.generate_proof(hash, hashes) == []:
             return False
         else:
             return True
 
     # helper functions
+
+    def _generate(self,hashes, tree):
+        if len(hashes) == 1:                            # if only one hash then return that only
+            return hashes
+            
+        self._ensureEven(hashes)                         # check if need for duplication
+        combinedHashes = []
+
+        for i in range(0, len(hashes), 2):                          # use every alternate node and concatenate it to create the parent hash
+            hashesConcatenated = hashes[i] + hashes[i + 1]
+            hash = self.hash(hashesConcatenated)
+            combinedHashes.append(hash)                             
+
+        tree.append(combinedHashes)                                 # append the levels of hashes to the tree
+        # print(tree)
+        return self._generate(combinedHashes, tree)
 
     def _get_hashlist(self, words:list):
         # returns a list of all the leaf nodes before building of the tree
@@ -103,18 +122,28 @@ class Merkletree:
             hashes.append(hash) 
         return hashes 
     
-    def getLeafNodeDirectionInMerkleTree(self, hash, merkleTree):
+    def _get_direction(self, hash, merkleTree):
         hashIndex = merkleTree[0].index(hash)
         if hashIndex % 2 == 0:
             return 'left'
         else:
             return 'right'
 
-    def ensureEven(self,lst):
+    def _ensureEven(self,lst):
         # duplicates if nodes are odd
         if len(lst) % 2 != 0:
             lst.append(lst[-1])
 
+# testing
 words = ["My", "name","is"]   
-tree = Merkletree(words)   
-print(tree.generateMerkleProof('8ed6791bdf3d61a1e6edcbb253979b0a6bef7f3d99dda0fb49cffe96923514b6',['8ed6791bdf3d61a1e6edcbb253979b0a6bef7f3d99dda0fb49cffe96923514b6', '82a3537ff0dbce7eec35d69edc3a189ee6f17d82f353a553f9aa96cb0be3ce89', 'fa51fd49abf67705d6a35d18218c115ff5633aec1f9ebfdc9d5d4956416f57f6']))
+tree = Merkletree(words)
+hashlist = ['8ed6791bdf3d61a1e6edcbb253979b0a6bef7f3d99dda0fb49cffe96923514b6', '82a3537ff0dbce7eec35d69edc3a189ee6f17d82f353a553f9aa96cb0be3ce89', 'fa51fd49abf67705d6a35d18218c115ff5633aec1f9ebfdc9d5d4956416f57f6']
+
+# does not exist in tree 
+print(tree.generate_proof('8ed6791bdf3d61a1e6edcbb253979b0a6bef7f3d99dda0fb49cffe96923514b6',hashlist))
+print(tree.verify('8ed6791bdf3d61a1e6edcbb253979b0a6bef7f3d99dda0fb49cffe96923514b6',hashlist))
+
+# exists in tree
+print(tree.generate_proof_using_value("My",hashlist))
+print(tree.generate_proof('8ed6791bdf3d61a1e6edcab253979b0a6bef7f3d99dda0fb49cffe96923514b6',hashlist))
+print(tree.verify('8ed6791bdf3d61a1e6edcab253979b0a6bef7f3d99dda0fb49cffe96923514b6',hashlist))
